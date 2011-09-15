2e7f0330e8e33a0e158184c08d4fdbcc21bc7f41
Michael Budde <mbudde@gmail.com>
2011-09-15 21:22:48 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python
 
+import os
 try:
     import subprocess
     has_subprocess = True
 except:
     has_subprocess = False
 
-from distutils.core import setup
+from setuptools import setup
 from distutils.cmd import Command
 
 import django_auth_iam
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
 
 class docs(Command):
 
@@ -36,18 +42,26 @@ class docs(Command):
         else:
             print '`setup.py docs` is not supported for this version of Python.'
 
+
 setup(
     name='django-auth-iam',
     version=django_auth_iam.__version__,
     description='Django authentication backend using Amazon IAM',
-    long_description='''This is a Django authentication backend that authenticates against the Amazon IAM service.''',
+    long_description=read('README.rst'),
     url='https://github.com/viewworld/django-auth-iam/',
     author='Michael Budde',
     author_email='mb@viewworld.dk',
-    license='BSD',
+    license='GPL v3',
     packages=['django_auth_iam'],
-    classifiers=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python',
+        'Topic :: Security',
+        'Topic :: System :: Systems Administration :: Authentication/Directory',
+    ],
     keywords=['django', 'amazon', 'authentication', 'auth'],
-    requires=['boto', 'PyCrypto', 'py_bcrypt'],
+    install_requires=['boto', 'PyCrypto', 'py_bcrypt'],
     cmdclass={'docs': docs}
 )
