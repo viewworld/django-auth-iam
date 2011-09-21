5328d411a9dabad1edac52d581b3ea22770bd258
Michael Budde <mbudde@gmail.com>
2011-09-21 11:40:44 +0200

    Documentation build command already built-in

--- setup.py
+++ setup.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
 
 import os
-try:
-    import subprocess
-    has_subprocess = True
-except:
-    has_subprocess = False
 
 from setuptools import setup
 from distutils.cmd import Command
@@ -15,33 +10,6 @@ import django_auth_iam
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
-
-
-class docs(Command):
-
-    description = "generate documentation"
-    user_options = []
-
-    def initialize_options(self): pass
-    def finalize_options(self): pass
-
-    def run(self):
-        path = 'docs/_build/{0}'.format(django_auth_iam.__version__)
-        mode = 'html'
-        try:
-            os.makedirs(path)
-        except:
-            pass
-        if has_subprocess:
-            status = subprocess.call(['sphinx-build', '-E', '-b', mode, 'docs', path])
-            if status:
-                raise RuntimeError("documentation step '{0}' failed".format(mode))
-            print ''
-            print "Documentation step '{0}' performed, results here:".format(mode)
-            print ' {0}/'.format(path)
-        else:
-            print '`setup.py docs` is not supported for this version of Python.'
-
 
 setup(
     name='django-auth-iam',
@@ -63,5 +31,4 @@ setup(
     ],
     keywords=['django', 'amazon', 'authentication', 'auth'],
     install_requires=['boto', 'PyCrypto', 'py_bcrypt'],
-    cmdclass={'docs': docs}
 )
